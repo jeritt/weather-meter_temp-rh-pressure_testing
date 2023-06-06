60eeb689107b261db4f6a901c0c37f4114982835
Jeritt Williams <49690937+jeritt@users.noreply.github.com>
2023-06-06 11:32:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
-function convertCtoF () {
+function convertPressure () {
+    hPa = Math.idiv(weatherbit.pressure(), 25600)
+}
+input.onButtonPressed(Button.A, function () {
+    convertTemp()
+    convertRH()
+    convertPressure()
+    serial.writeValue("Temp F", tempCelsius)
+    serial.writeValue("Humidity %", RH)
+    serial.writeValue("hPa", hPa)
+})
+function convertTemp () {
     tempCelsius = Math.idiv(weatherbit.temperature(), 100)
     tempFarenheit = tempCelsius * 1.8 + 32
 }
-input.onButtonPressed(Button.A, function () {
-    convertCtoF()
-    convertRH()
-    convertmBar()
-    serial.writeValue("Temp F", tempFarenheit)
-    serial.writeValue("Humidity %", RH)
-    serial.writeValue("Pressure mBar", mBar)
-})
 function convertRH () {
     RH = Math.idiv(weatherbit.humidity(), 1024)
 }
-function convertmBar () {
-    mBar = Math.idiv(weatherbit.pressure(), 25600)
-}
-let mBar = 0
-let RH = 0
 let tempFarenheit = 0
+let RH = 0
 let tempCelsius = 0
+let hPa = 0
 weatherbit.startWeatherMonitoring()
 basic.forever(function () {
 	
