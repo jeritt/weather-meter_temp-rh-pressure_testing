f473b3bd99c93bd374678454b9cfd340fdd7e874
Jeritt Williams <49690937+jeritt@users.noreply.github.com>
2023-06-07 06:18:32 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
-function convertPressure () {
-    hPa = Math.idiv(weatherbit.pressure(), 25600)
+function fixWindDir () {
+    if (weatherbit.windDirection() != "???") {
+        currentWindDirection = weatherbit.windDirection()
+    }
 }
 input.onButtonPressed(Button.A, function () {
     convertTemp()
     convertRH()
-    convertPressure()
-    serial.writeValue("Temp F", tempCelsius)
-    serial.writeValue("Humidity %", RH)
-    serial.writeValue("hPa", hPa)
+    fixWindDir()
+    serial.writeLine(timeanddate.dateTime())
+    serial.writeValue("Temp (C)", tempCelsius)
+    serial.writeValue("Humidity (%)", RH)
+    serial.writeValue("Dew Point", BME280.Dewpoint())
+    serial.writeValue("Pressure (hPa)", BME280.pressure(BME280_P.hPa))
+    serial.writeValue("Wind Speed (mph)", Math.trunc(weatherbit.windSpeed()))
+    serial.writeLine("Wind Direction: " + currentWindDirection)
 })
 function convertTemp () {
     tempCelsius = Math.idiv(weatherbit.temperature(), 100)
@@ -19,8 +25,9 @@ function convertRH () {
 let tempFarenheit = 0
 let RH = 0
 let tempCelsius = 0
-let hPa = 0
+let currentWindDirection = ""
+timeanddate.setDate(6, 6, 2023)
+timeanddate.set24HourTime(14, 53, 0)
+serial.redirectToUSB()
 weatherbit.startWeatherMonitoring()
-basic.forever(function () {
-	
-})
+weatherbit.startWindMonitoring()
